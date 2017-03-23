70e2884ef5c2778ed0b696c7d249a21a190304c7
TestOodoO <prakashsankark@gmail.com>
2017-03-23 21:11:09 +0530

    before existing image ignore

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 $filepath = 'docs' ;
 
+//https://www.youtube.com/watch?v=23KufSqo6cQ
+
 $server = "localhost";
 $dbusername = "root";
 $dbpassword = "1projectK!";
@@ -12,45 +14,19 @@ $con = new mysqli($server, $dbusername, $dbpassword, $dbname);
 		var_dump("Connection error: ". $con->connect_error );
 	}
 
-	if(mysqli_query($con, "DESCRIBE `device_details`")) {
-		$result = mysqli_query($con, "SELECT device_id FROM device_details order by id desc");
-		$num_rows = mysqli_num_rows($result);
-		if($num_rows >= 1){
-			$device_id = $result->fetch_object()->device_id;
-			$device_no = intval(preg_replace('/[^0-9]+/', '', $device_id), 10)+1;
-			$device_id = 'OFONU'.$device_no;
-		}else{
-			$device_id = 'OFONU1087600';
-		}
-	}else{
-		$sql = "CREATE TABLE device_details (
-		id int NOT NULL AUTO_INCREMENT,
-		name varchar(255) NOT NULL,
-		device_id varchar(20) NOT NULL,
-		serial_no varchar(255),
-		mac_address varchar(255),
-		image_id varchar(255),
-		latitude double(11,8) NOT NULL,
-		longitude double(11,8) NOT NULL,
-		latitude1 varchar(255) NOT NULL,
-		longitude1 varchar(255) NOT NULL,
-		primary key(id) )";
-
-		if(mysqli_query($con, $sql)) {
-			echo "Table Created successfully";
-		}else{
-			echo "Error Creating Table :". mysqli_error($con);
-		}
-		$device_id = 'OFCHN1087600';
-	}
-
 
 
 	if($handle = opendir($filepath)) {
 		while( false !== ($file = readdir($handle))) {
 			if(is_dir($file) == true){
 				break;
-			}
+			}	
+			$path = realpath(dirname(__FILE__).'/docs');
+			$now = $path.'/'.$file;
+			//var_dump($now); die;		
+/*			$img_exist = mysqli_query($con, "SELECT * FROM device_details WHERE name = '.$file.'");
+			$count = mysqli_num_rows($img_exist);*/
+			//var_dump($count); die;
 			$first_lat = substr($file, strpos($file, "___")+3);
 			$first_long = substr($first_lat, strpos($first_lat, "_")+1);
 			$sec_lat_long = substr($first_long, strpos($first_long, "__")+2);
@@ -59,7 +35,51 @@ $con = new mysqli($server, $dbusername, $dbpassword, $dbname);
 			$longitude = floatval($first_long);
 			$latitude1 = $arr[0];
 			$longitude1 = $arr[1];
-			//var_dump($latitude, $longitude,$latitude1,$longitude1);
+
+			if(mysqli_query($con, "DESCRIBE `device_details`")) {
+				$result = mysqli_query($con, "SELECT device_id FROM device_details order by id desc");
+				$num_rows = mysqli_num_rows($result);
+				if($num_rows >= 1){
+					$device_id = $result->fetch_object()->device_id;
+					$device_no = intval(preg_replace('/[^0-9]+/', '', $device_id), 10)+1;
+					$device_id = 'OFONU'.$device_no;
+				}else{
+					$device_id = 'OFONU1087600';
+				}
+			}else{
+				$sql = "CREATE TABLE device_details (
+				id int NOT NULL AUTO_INCREMENT,
+				image_name varchar(255) NOT NULL,
+				device_id varchar(20) NOT NULL,
+				serial_no varchar(255),
+				mac_address varchar(255),
+				image_id varchar(255),
+				latitude double(11,8) NOT NULL,
+				longitude double(11,8) NOT NULL,
+				latitude1 varchar(255) NOT NULL,
+				longitude1 varchar(255) NOT NULL,
+				primary key(id)) DEFAULT CHARSET=utf8";
+
+				if(mysqli_query($con, $sql)) {
+					echo "Table Created successfully";
+				}else{
+					echo "Error Creating Table :". mysqli_error($con);
+					break;
+				}
+				$device_id = 'OFCHN1087600';
+			}
+			$image_id = hash('sha256',$file);
+			$image[] = array(
+				'image_id' => $image_id,
+				'image_location' => realpath(dirname(__FILE__).'/docs/').$file
+			);
+			$sql = "INSERT INTO device_details (image_name, device_id, image_id, latitude, longitude, latitude1, longitude1)
+			VALUES ('$file', '$device_id', '$image_id', '$latitude', '$longitude', '$latitude1', '$longitude1')";
+			if(mysqli_query($con, $sql)){
+				echo "Device Detail Added ".$file;
+			}else{
+				echo "Error Adding Details :". mysqli_error($con); 
+			}
 		}
 	}
 
